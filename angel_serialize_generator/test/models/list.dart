da208d71e3c8d06ba23fd0a172b3cbf498f6312b
Kirill Danilov <hello@kirilldanilov.com>
2020-02-13 20:55:37 +0300

    commented nested classes, use import, test failed

--- angel_serialize_generator/test/models/list.dart
+++ angel_serialize_generator/test/models/list.dart
@@ -3,19 +3,19 @@ import 'package:collection/collection.dart';
 
 part 'list.g.dart';
 
-@serializable
-class _Animal {
-  @notNull
-  String genus;
-  @notNull
-  String species;
-}
+// @serializable
+// class _Animal {
+//   @notNull
+//   String genus;
+//   @notNull
+//   String species;
+// }
 
-@serializable
-class _Bird extends _Animal {
-  @DefaultsTo(false)
-  bool isSparrow;
-}
+// @serializable
+// class _Bird extends _Animal {
+//   @DefaultsTo(false)
+//   bool isSparrow;
+// }
 
 @serializable
 class _Zoo {
